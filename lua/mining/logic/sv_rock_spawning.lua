f6af75aedc5673817b0c8aa60eba73646aa15ce8
TW1STaL1CKY <tw1stal1cky@gmail.com>
2020-12-27 22:30:40 +0000

    Tone down Xen crystal spawn chance because now it's too much

--- lua/mining/logic/sv_rock_spawning.lua
+++ lua/mining/logic/sv_rock_spawning.lua
@@ -280,8 +280,8 @@ end
 local function TrySpawnCrystal()
     if not (mapdata.minespots and next(mapdata.minespots)) then return end
 
-    -- 25% chance to spawn in every minute
-    if math.random() <= 0.25 then return end
+    -- 10% chance to spawn in every minute
+    if math.random() <= 0.1 then return end
 
 	-- 4 attempts
 	for i=1,4 do
