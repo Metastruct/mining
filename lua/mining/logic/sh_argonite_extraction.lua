368b42c60a5a5dd16d112a11473a0004126bb157
Earu <earu.derg@gmail.com>
2022-09-17 20:13:48 +0200

    tweaks

--- lua/mining/logic/sh_argonite_extraction.lua
+++ lua/mining/logic/sh_argonite_extraction.lua
@@ -373,11 +373,12 @@ if SERVER then
 			local tr = util.TraceLine({
 				start = localPos,
 				endpos = localEndPos,
+				mask = MASK_SOLID_BRUSHONLY,
 			})
 
 			if tr.Hit and util.IsInWorld(tr.HitPos) then
 				local rock = ents.Create("mining_rock")
-				rock:SetPos(tr.HitPos)
+				rock:SetPos(tr.HitPos + tr.HitNormal * 10)
 				rock:SetSize(math.random() > 0.33 and 1 or 2)
 				rock:SetRarity(ARGONITE_RARITY)
 				rock:Spawn()
