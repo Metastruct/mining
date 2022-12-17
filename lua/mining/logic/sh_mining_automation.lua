ad66d92d5d85b20d13a1e2e0accd8419cf9c84fa
Earu <earu.derg@gmail.com>
2022-12-17 13:09:58 +0100

    ignore that

--- lua/mining/logic/sh_mining_automation.lua
+++ lua/mining/logic/sh_mining_automation.lua
@@ -13,6 +13,7 @@ Ores.Automation = {
 		mining_drill = true,
 		mining_conveyor_splitter = true,
 		mining_ore_smelter = true,
+		mining_coin_minter = true,
 	},
 	BaseOreProductionRate = 10, -- 1 per 10 seconds
 	EnergyMaterial = Material("models/props_combine/coredx70"),
