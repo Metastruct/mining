eb151119fad26a9f82aaf97fa4a86de167ea1a22
Ryan <ryan.rbsr@gmail.com>
2022-12-22 01:01:56 +0100

    oil extractor to make fuel tanks with argonite batteries

---
+++ lua/entities/mining_oil_extractor.lua
@@ -0,0 +1,264 @@
+AddCSLuaFile()
+
+module("ms", package.seeall)
+Ores = Ores or {}
+
+ENT.Type = "anim"
+ENT.Base = "base_anim"
+ENT.PrintName = "Oil Extractor"
+ENT.Author = "Earu"
+ENT.Category = "Mining"
+ENT.RenderGroup = RENDERGROUP_OPAQUE
+ENT.Spawnable = true
+ENT.ClassName = "mining_oil_extractor"
+ENT.NextTraceCheck = 0
+
+local function can_work(self, time)
+	if not self:GetNWBool("IsPowered", true) then return false end
+	if time < self.NextTraceCheck then return self.TraceCheckResult end
+
+	if self:GetNW2Int("Energy", 0) > 0 then
+		local tr = util.TraceLine({
+			start = self:GetPos() + self:GetUp() * -75,
+			endpos = self:GetPos() + self:GetUp() * -100,
+			mask = MASK_SOLID_BRUSHONLY,
+		})
+
+		self.NextTraceCheck = time + 1.5
+		self.TraceCheckResult = tr.Hit
+		return tr.Hit
+	end
+
+	self.NextTraceCheck = time + 1.5
+	self.TraceCheckResult = false
+	return false
+end
+
+local EXTRACTION_RATE = 10 * 60 -- 1 fuel tank per 10 mins
+if SERVER then
+	ENT.NextSoundCheck = 0
+
+	function ENT:Initialize()
+		self:SetModel("models/props_wasteland/coolingtank02.mdl")
+		self:SetMaterial("phoenix_storms/future_vents")
+		self:SetModelScale(0.4)
+		self:SetMoveType(MOVETYPE_VPHYSICS)
+		self:PhysicsInit(SOLID_VPHYSICS)
+		self:SetSolid(SOLID_VPHYSICS)
+		self:Activate()
+		self:PhysWake()
+		self.NextSoundCheck = 0
+		self.NextTraceCheck = 0
+		self:SetNWBool("IsPowered", true)
+
+		self.Frame = ents.Create("prop_physics")
+		self.Frame:SetModel("models/props_phx/construct/metal_wire1x1x2.mdl")
+		self.Frame:SetMaterial("phoenix_storms/future_vents")
+		self.Frame:SetPos(self:GetPos() + self:GetForward() * 24 + self:GetUp() * -35)
+
+		local ang = self:GetAngles()
+		ang:RotateAroundAxis(self:GetRight(), 90)
+
+		self.Frame:SetAngles(ang)
+		self.Frame:Spawn()
+		self.Frame:SetParent(self)
+
+		self.Out = ents.Create("prop_physics")
+		self.Out:SetModel("models/props_phx/construct/metal_wire1x1.mdl")
+		self.Out:SetMaterial("phoenix_storms/stripes")
+		self.Out:SetPos(self:GetPos() + self:GetRight() * 24 + self:GetUp() * -35)
+
+		ang = self:GetAngles()
+		--ang:RotateAroundAxis(self:GetRight(), 90)
+		ang:RotateAroundAxis(self:GetForward(), 90)
+
+		self.Out:SetAngles(ang)
+		self.Out:Spawn()
+		self.Out:SetParent(self)
+		self.Out:SetNotSolid(true)
+
+		timer.Simple(0, function()
+			if not IsValid(self) then return end
+
+			self:Activate()
+			Ores.Automation.ReplicateOwnership(self, self)
+			self.SndLoop = self:StartLoopingSound("ambient/machines/transformer_loop.wav")
+		end)
+
+		if _G.WireLib then
+			_G.WireLib.CreateInputs(self, {
+				"Active",
+			}, {
+				"Whether the extractor is active or not",
+			})
+		end
+
+		Ores.Automation.PrepareForDuplication(self)
+		Ores.Automation.RegisterEnergyPoweredEntity(self, {
+			{
+				Type = "Energy",
+				MaxValue = Ores.Automation.BatteryCapacity * 3,
+				ConsumptionRate = 10, -- 1 unit every 10 seconds
+			}
+		})
+	end
+
+	function ENT:TriggerInput(port, state)
+		if not _G.WireLib then return end
+		if not isnumber(state) then return end
+
+		if port == "Active" then
+			self:SetNWBool("IsPowered", tobool(state))
+		end
+	end
+
+	function ENT:CheckSoundLoop(time)
+		if time < self.NextSoundCheck then return end
+
+		if not can_work(self, time) then
+			if self.SndLoop and self.SndLoop ~= -1 then
+				self:StopLoopingSound(self.SndLoop)
+			end
+
+			self.SndLoop = nil
+			self.NextSoundCheck = time + 5
+			return
+		end
+
+		if not self.SndLoop or self.SndLoop == -1 then
+			self.SndLoop = self:StartLoopingSound("ambient/machines/transformer_loop.wav")
+		end
+
+		self.NextSoundCheck = time + 5
+	end
+
+	function ENT:CanConsumeEnergy()
+		if not can_work(self, CurTime()) then return false end
+
+		return true
+	end
+
+	function ENT:ExtractOil(time)
+		if time < self:GetNWInt("NextOil", 0) then return end
+		if not can_work(self, time) then return end
+
+		local fuelTank = ents.Create("mining_fuel_tank")
+		fuelTank:SetPos(self:GetPos() + self:GetRight() * 50 + self:GetUp() * -45)
+		fuelTank:SetNWInt("CoalCount", 150)
+		fuelTank:Spawn()
+		fuelTank:PhysWake()
+
+		timer.Simple(0, function()
+			if IsValid(self) and IsValid(fuelTank) then
+				Ores.Automation.ReplicateOwnership(fuelTank, self, true)
+			end
+		end)
+
+		self:SetNWInt("NextOil", time + EXTRACTION_RATE)
+	end
+
+	function ENT:Think()
+		local time = CurTime()
+		self:CheckSoundLoop(time)
+		self:ExtractOil(time)
+	end
+
+	function ENT:OnRemove()
+		if self.SndLoop and self.SndLoop ~= -1 then
+			self:StopLoopingSound(self.SndLoop)
+		end
+	end
+end
+
+if CLIENT then
+	local WHEEL_MDL = "models/props_wasteland/wheel01.mdl"
+	local function addWheelEntity(self, offset)
+		local wheel = ClientsideModel(WHEEL_MDL)
+		wheel:SetPos(self:GetPos() + offset)
+		wheel:Spawn()
+		wheel:SetParent(self)
+
+		local argoniteRarity = Ores.Automation.GetOreRarityByName("Argonite")
+		wheel.RenderOverride = function()
+			local color = Ores.__R[argoniteRarity].PhysicalColor
+			render.SetColorModulation(color.r / 100, color.g / 100, color.b / 100)
+			render.MaterialOverride(Ores.Automation.EnergyMaterial)
+			wheel:DrawModel()
+			render.MaterialOverride()
+			render.SetColorModulation(1, 1, 1)
+		end
+
+		return wheel
+	end
+
+	function ENT:Initialize()
+		self.NextTraceCheck = 0
+		self.Wheel = addWheelEntity(self, self:GetUp() * -75)
+	end
+
+	local EFFECT_NAME = "WheelDust"
+	function ENT:Draw()
+		self:DrawModel()
+
+		local time = CurTime()
+		local hasEnergy = can_work(self, time)
+		if hasEnergy then
+			local effectData = EffectData()
+			effectData:SetScale(1.5)
+			effectData:SetOrigin(self:GetPos() + self:GetUp() * -75)
+			util.Effect(EFFECT_NAME, effectData)
+		end
+
+		if IsValid(self.Wheel) then
+			local ang = self:GetAngles()
+			ang:RotateAroundAxis(self:GetForward(), 90)
+
+			if hasEnergy then
+				self.Wheel:SetPos(self:GetPos() + self:GetUp() * (-70 + math.abs(math.sin(CurTime() * -1)) * 10))
+				ang:RotateAroundAxis(self:GetUp(), CurTime() * 300 % 360)
+			else
+				self.Wheel:SetPos(self:GetPos() + self:GetUp() * -70)
+			end
+			self.Wheel:SetParent(self)
+
+			self.Wheel:SetAngles(ang)
+		end
+	end
+
+	function ENT:OnRemove()
+		SafeRemoveEntity(self.Wheel)
+	end
+
+	local OIL_MAT = Material("models/shadertest/shader4")
+	function ENT:OnGraphDraw(x, y)
+		local GU = Ores.Automation.GraphUnit
+
+		surface.SetDrawColor(255, 255, 255, 255)
+		surface.SetMaterial(OIL_MAT)
+		surface.DrawTexturedRect(x - GU / 2, y - GU / 2, GU, GU)
+
+		surface.SetDrawColor(125, 125, 125, 255)
+		surface.DrawOutlinedRect(x - GU / 2, y - GU / 2, GU, GU, 2)
+
+		surface.SetTextColor(255, 255, 255, 255)
+		local perc = (math.Round((self:GetNW2Int("Energy", 0) / self:GetNW2Int("MaxEnergy", Ores.Automation.BatteryCapacity * 3)) * 100)) .. "%"
+		surface.SetFont("DermaDefault")
+		local tw, th = surface.GetTextSize(perc)
+		surface.SetTextPos(x - tw / 2, y - th / 2)
+		surface.DrawText(perc)
+	end
+
+	function ENT:OnDrawEntityInfo()
+		if not self.MiningFrameInfo then
+			self.MiningFrameInfo = {
+				{ Type = "Label", Text = "EXTRACTOR", Border = true },
+				{ Type = "Data", Label = "ENERGY", Value = self:GetNW2Int("Energy", 0), MaxValue = self:GetNW2Int("MaxEnergy", Ores.Automation.BatteryCapacity * 3), Border = true },
+				{ Type = "Data", Label = "OIL", Value = math.max(0, self:GetNWInt("NextOil", 0) - CurTime()), MaxValue = EXTRACTION_RATE },
+			}
+		end
+
+		self.MiningFrameInfo[2].Value = self:GetNW2Int("Energy", 0)
+		self.MiningFrameInfo[3].Value = EXTRACTION_RATE - math.max(0, self:GetNWInt("NextOil", 0) - CurTime())
+		return self.MiningFrameInfo
+	end
+end
