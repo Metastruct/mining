e01d8f1d86d4fca10bfd58ec6bd2ef7781db893e
Ryan <ryan.rbsr@gmail.com>
2023-08-05 13:06:05 +0200

    drill -> transformer

--- lua/entities/mining_argonite_transformer.lua
+++ lua/entities/mining_argonite_transformer.lua
@@ -146,7 +146,7 @@ if SERVER then
 		end)
 
 		if _G.WireLib then
-			self.Inputs = _G.WireLib.CreateInputs(self, {"Active (If this is non-zero, activate the drill)"})
+			self.Inputs = _G.WireLib.CreateInputs(self, {"Active (If this is non-zero, activate the transformer)"})
 
 			_G.WireLib.CreateOutputs(self, {
 				"Amount (Outputs the current amount of argonite filled in) [NORMAL]",
