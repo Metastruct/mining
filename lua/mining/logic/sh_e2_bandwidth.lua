668703b59789ed90db347066b7b622774a07cb0e
Ryan <ryan.rbsr@gmail.com>
2023-08-07 22:11:57 +0200

    [wip] allow e2 and mining together but powered by detonite

---
+++ lua/mining/logic/sh_e2_bandwidth.lua
@@ -0,0 +1,142 @@
+local tag = "mining_e2_chip_bandwidth"
+local MAX_CUCKAGE = 300
+
+if SERVER then
+	local function only_e2_chips(ply)
+		for ent, _ in pairs(ply._miningChipsOwned or {}) do
+			if ent:GetClass() ~= "gmod_wire_expression2" then return false end
+		end
+
+		return true
+	end
+
+	local function get_e2data()
+		local e2data = {}
+
+		for _, e2 in pairs(ents.FindByClass("gmod_wire_expression2")) do
+			local ops = e2.OverlayData.prfbench
+			local owner = e2:GetPlayer()
+
+			if not e2data[owner] then
+				e2data[owner] = 0
+			end
+
+			e2data[owner] = e2data[owner] + ops
+		end
+
+		return e2data
+	end
+
+	local e2_owners = {}
+	E2Lib.registerCallback("construct", function(data)
+		if data.player:GetNWInt(tag, 0) <= 0 then
+			data.player:SetNWInt(tag, 0)
+		end
+
+		e2_owners[data.player] = true
+
+		timer.Simple(1, function()
+			if not IsValid(data.player) then return end
+			if not IsValid(data.enttity) then return end
+
+			data.player._miningChipsOwned[data.entity] = false
+
+			if only_e2_chips(data.player) and data.player:GetNWInt(tag, 0) > 0 then
+				data.player._miningBlocked = false
+			end
+		end)
+	end)
+
+	hook.Add("EntityRemoved", tag, function(ent)
+		if ent:GetClass() ~= "gmod_wire_expression2" then return end
+
+		local owner = ent:GetPlayer()
+		if not get_e2data()[owner] then
+			e2_owners[owner] = nil
+			--owner:SetNWInt(tag, -1)
+		end
+	end)
+
+	hook.Add("PlayerReceivedOre", tag, function(ply, amount, rarity)
+		local detonite_rarity = ms.Ores.Automation.GetOreRarityByName("Detonite")
+		if rarity ~= detonite_rarity then return end
+
+		local cur_detonite = ply:GetNWInt(tag, 0)
+		if cur_detonite >= MAX_CUCKAGE then return end
+
+		ply:SetNWInt(tag, math.min(cur_detonite + amount, MAX_CUCKAGE))
+		ms.Ores.TakePlayerOre(ply, detonite_rarity, amount)
+	end)
+
+	hook.Add("OnEntityCreated", tag, function(ent)
+		if ent:GetClass() ~= "mining_ore" then return end
+
+		timer.Simple(0, function()
+			local detonite_rarity = ms.Ores.Automation.GetOreRarityByName("Detonite")
+			local rarity = ent:GetRarity()
+
+			if rarity ~= detonite_rarity then return end
+
+			local old_touch = ent.Touch
+			function ent:Touch(e)
+				if e:IsPlayer() and e._miningBlocked then
+					self:Consume(e)
+					return
+				end
+
+				old_touch(self, e)
+			end
+		end)
+	end)
+
+	timer.Create(tag, 10, 0, function()
+		local data = get_e2data()
+		for owner, total_ops in pairs(data) do
+			local owned_detonite = owner:GetNWInt(tag, 0)
+			local required_detonite = math.max(1, math.ceil(total_ops / 100))
+			if owned_detonite >= required_detonite then
+				print(owner, owned_detonite, required_detonite)
+				owner:SetNWInt(tag, math.max(0, owned_detonite - required_detonite))
+
+				if only_e2_chips(owner) then
+					owner._miningBlocked = false
+				end
+			else
+				for ent, _ in pairs(owner._miningChipsOwned or {}) do
+					owner._miningChipsOwned[ent] = true
+					ent:EmitSound("ambient/machines/thumper_shutdown1.wav")
+				end
+
+				if only_e2_chips(owner) then
+					owner._miningBlocked = true
+				end
+			end
+		end
+	end)
+end
+
+if CLIENT then
+	hook.Add("HUDPaint", tag, function()
+		local ply = LocalPlayer()
+		if not ply.IsInZone then return end
+		if not ply:IsInZone("cave") then return end
+
+		local owned_detonite = ply:GetNWInt(tag, -1)
+		if owned_detonite < 0 then return end
+
+		local X, Y = ScrW() * 3 / 4, ScrH() - 120
+
+		surface.SetMaterial(ms.Ores.Automation.HudFrameMaterial)
+		surface.SetDrawColor(255, 255, 255, 255)
+		surface.DrawTexturedRect(X, Y, 250, 110)
+
+		surface.SetTextColor(255, 0, 0, 255)
+		surface.SetFont("mining_automation_hud")
+
+		surface.SetTextPos(X + 25, Y + 15)
+		surface.DrawText("Chip Bandwidth")
+
+		surface.SetTextPos(X + 25, Y + 55)
+		surface.DrawText(("%d/%d"):format(owned_detonite,MAX_CUCKAGE))
+	end)
+end
--- lua/mining/logic/sv_anticheat.lua
+++ lua/mining/logic/sv_anticheat.lua
@@ -1,20 +1,19 @@
 module("ms", package.seeall)
 local Tag = "mineanticheat"
 local Ores = assert(Ores)
-
-local maxMinerRange = 340*340
+local maxMinerRange = 340 * 340
 
 local function playerNearMiningNPC(pl)
 	local pos = pl:GetPos()
 
-	for k,npc in next,ents.FindByClass("lua_npc") do
+	for k, npc in next, ents.FindByClass("lua_npc") do
 		if npc.role == "miner" and npc:GetPos():DistToSqr(pos) < maxMinerRange then return npc end
 	end
 end
 
 local function playerHasOres(pl)
-	for k,v in next,Ores.__R do
-		local n = Ores.GetPlayerOre(pl,k)
+	for k, v in next, Ores.__R do
+		local n = Ores.GetPlayerOre(pl, k)
 		if n > 0 then return true end
 	end
 end
@@ -23,28 +22,22 @@ FindMetaTable("Player").HasOres = playerHasOres
 
 -- Anti-cheat Hooks
 -- Hinder mining bots by setting a cooldown where rocks and ores can't be interacted with, normal players shouldn't notice this
-
 --TODO: dont block if last mine action was ages ago!!
-
 local function applyMiningCooldown(pl)
 	if not pl._receivedOre then return end
-
 	local now = CurTime()
-
 	-- This forgives if you haven't mined yet
 	local secondsSinceLastMineAction = now - (pl._lastMiningAction or -999999)
-
 	if secondsSinceLastMineAction > 60 * 5 then return end
-
 	-- This forgives once every 8 minutes and warns on the first try
 	local secondsSinceLastForgiveCooldown = now - (pl._miningNoclipForgiveTimer or -999999)
+
 	if secondsSinceLastForgiveCooldown > 60 * 8 then
 		pl._miningNoclipForgiveTimer = now
 
 		if not pl._miningNoclipForgiveMsgd then
 			pl._miningNoclipForgiveMsgd = true
-
-			Ores.SendChatMessage(pl,1,"WARNING: Noclipping/teleporting prevents mining for a while!")
+			Ores.SendChatMessage(pl, 1, "WARNING: Noclipping/teleporting prevents mining for a while!")
 			pl:EmitSound("vo/npc/female01/thehacks02.wav")
 		end
 
@@ -58,55 +51,51 @@ local function setLastMiningAction(pl)
 	pl._lastMiningAction = CurTime()
 end
 
-hook.Add("PlayerNoClip",Tag,function(pl,enable)
+hook.Add("PlayerNoClip", Tag, function(pl, enable)
 	if not enable then
 		applyMiningCooldown(pl)
 	end
 end)
 
-hook.Add("PlayerDestroyedMiningRock",Tag,setLastMiningAction)
-hook.Add("PlayerReceivedOre",Tag,setLastMiningAction)
-
+hook.Add("PlayerDestroyedMiningRock", Tag, setLastMiningAction)
+hook.Add("PlayerReceivedOre", Tag, setLastMiningAction)
 --TODO
-hook.Add("PlayerSoldOre",Tag,setLastMiningAction)
+hook.Add("PlayerSoldOre", Tag, setLastMiningAction)
 
-hook.Add("CanPlyGoto",Tag,function(pl)
+hook.Add("CanPlyGoto", Tag, function(pl)
 	if not pl:IsPlayer() then return end
 	if not pl._receivedOre then return end
-
 	applyMiningCooldown(pl)
-
 	if not playerHasOres(pl) then return end
 	if playerNearMiningNPC(pl) then return end
 	-- must block trading here before timer, otherwise race condition
 	pl._receivedOre = false
 
-	timer.Create("ms.Ores_CheckMiningNPC"..pl:EntIndex(),0.1,1,function()
+	timer.Create("ms.Ores_CheckMiningNPC" .. pl:EntIndex(), 0.1, 1, function()
 		pl._receivedOre = true
 		local npc = playerNearMiningNPC(pl)
 
 		if npc then
-			timer.Simple(math.Rand(0.01,0.1),function()
+			timer.Simple(math.Rand(0.01, 0.1), function()
 				npc:EmitSound("vo/npc/female01/startle01.wav")
 
-				timer.Simple(math.Rand(0.2,0.5),function()
+				timer.Simple(math.Rand(0.2, 0.5), function()
 					npc:EmitSound(")vo/npc/male01/thehacks02.wav")
 				end)
 			end)
 
-			Ores.SendChatMessage(pl,1,"The merchant was startled by your sudden appearance and refuses to accept your ores at this time...")
+			Ores.SendChatMessage(pl, 1, "The merchant was startled by your sudden appearance and refuses to accept your ores at this time...")
 			pl._receivedOre = false
 		end
 	end)
 end)
 
-hook.Add("CanPlyTeleport",Tag,applyMiningCooldown)
+hook.Add("CanPlyTeleport", Tag, applyMiningCooldown)
 
 util.OnInitialize(function()
-	local function addToChipsOwned(pl,ent)
+	local function addToChipsOwned(pl, ent)
 		pl._miningChipsOwned = pl._miningChipsOwned or {}
 		pl._miningChipsOwned[ent] = true
-
 		pl._miningBlocked = true
 	end
 
@@ -116,9 +105,9 @@ util.OnInitialize(function()
 		local registerCallback = _G.E2Lib.registerCallback or registerCallback
 
 		if registerCallback then
-			registerCallback("construct",function(data)
+			registerCallback("construct", function(data)
 				if data.player and data.player:IsValid() then
-					addToChipsOwned(data.player,data.entity)
+					addToChipsOwned(data.player, data.entity)
 				end
 			end)
 
@@ -127,28 +116,25 @@ util.OnInitialize(function()
 	end
 
 	if istable(_G.SF) then
-		hook.Add("PlayerLoadedStarfall","ms.Ores_ChipChecks",function(pl,ent,mainFile,allFiles)
-			addToChipsOwned(pl,ent)
+		hook.Add("PlayerLoadedStarfall", "ms.Ores_ChipChecks", function(pl, ent, mainFile, allFiles)
+			addToChipsOwned(pl, ent)
 		end)
 
 		chipClasses.starfall_processor = true
 	end
 
 	if next(chipClasses) then
-		hook.Add("EntityRemoved","ms.Ores_ChipChecks",function(ent)
+		hook.Add("EntityRemoved", "ms.Ores_ChipChecks", function(ent)
 			if not chipClasses[ent:GetClass()] then return end
-
 			local pl = ent.owner or ent.player or (ent.CPPIGetOwner and ent:CPPIGetOwner())
 
 			if pl and pl:IsValid() and pl._miningChipsOwned then
 				pl._miningChipsOwned[ent] = nil
-
 				-- If the player still has chips spawned, don't remove the _miningBlocked flag yet
 				if next(pl._miningChipsOwned) then return end
-
 				pl._miningChipsOwned = nil
 				pl._miningBlocked = nil
-				pl._miningCooldown = CurTime()+30
+				pl._miningCooldown = CurTime() + 30
 			end
 		end)
 	else
