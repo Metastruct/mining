29825118a29c0c33192d93b8aba65d13ef9c5e93
Ryan <ryan.rbsr@gmail.com>
2023-09-03 00:27:35 +0200

    ores dont need to be triggers anymore

--- lua/entities/mining_ore/init.lua
+++ lua/entities/mining_ore/init.lua
@@ -1,12 +1,11 @@
 include("shared.lua")
 AddCSLuaFile("shared.lua")
 AddCSLuaFile("cl_init.lua")
-
 ENT.ExpiryCheckPlayers = true
-
 ENT.ms_nogoto = "No cheating!"
 ENT._initialized = false
 ENT._screamsNext = 0
+
 ENT._screams = {
 	")ambient/voices/f_scream1.wav",
 	")vo/npc/female01/gethellout.wav",
@@ -30,21 +29,18 @@ ENT._screams = {
 
 function ENT:Initialize()
 	self._initialized = true
-
 	self:SetModel("models/props_combine/breenbust_chunk05.mdl")
-
 	self:PhysicsInit(SOLID_VPHYSICS)
 	self:SetSolid(SOLID_VPHYSICS)
 	self:SetMoveType(MOVETYPE_VPHYSICS)
 	self:SetCollisionGroup(COLLISION_GROUP_WEAPON)
-	self:SetTrigger(true)
-	self:UseTriggerBounds(true,6)
-
+	--self:SetTrigger(true)
+	--self:UseTriggerBounds(true,6)
 	self.ms_notouch = true
-	self.Expiry = CurTime()+180
+	self.Expiry = CurTime() + 180
 
 	if self.AllowTouch == nil then
-		timer.Simple(0.5,function()
+		timer.Simple(0.5, function()
 			if self:IsValid() and not self._consumed and self.AllowTouch == nil then
 				self:SetAllowTouch(true)
 			end
@@ -54,34 +50,29 @@ function ENT:Initialize()
 	self:DrawShadow(false)
 	self:SetMaterial("models/shiny")
 
-	timer.Simple(1.25,function()
-		if self:IsValid() then self:CheckExistence() end
+	timer.Simple(1.25, function()
+		if self:IsValid() then
+			self:CheckExistence()
+		end
 	end)
 end
 
-function ENT:Touch(ent)
+--[[function ENT:Touch(ent)
 	local now = CurTime()
 
-	if self.AllowTouch
-	and not self:IsPlayerHolding()
-	and ent:IsPlayer()
-	and ent:GetMoveType() != MOVETYPE_NOCLIP
-	and not ent._miningBlocked
-	and now >= (ent._miningCooldown or 0)
-	and not (ent.IsAFK and ent:IsAFK())
-	and (self.GraceOwner == NULL or (self.GraceOwner == ent or now >= (self.GraceOwnerExpiry or 0))) then
+	if self.AllowTouch and not self:IsPlayerHolding() and ent:IsPlayer() and ent:GetMoveType() ~= MOVETYPE_NOCLIP and not ent._miningBlocked and now >= (ent._miningCooldown or 0) and not (ent.IsAFK and ent:IsAFK()) and (self.GraceOwner == NULL or (self.GraceOwner == ent or now >= (self.GraceOwnerExpiry or 0))) then
 		if banni and banni.isbanned(ent) then
 			local phys = self:GetPhysicsObject()
-			if phys:IsValid() then
-				local dir = (self:GetPos()-ent:GetPos()):GetNormalized()
-				dir.z = 0
 
+			if phys:IsValid() then
+				local dir = (self:GetPos() - ent:GetPos()):GetNormalized()
+				dir.z = 0
 				phys:Wake()
-				phys:AddVelocity(dir*64)
+				phys:AddVelocity(dir * 64)
 
 				if now >= self._screamsNext then
-					self:EmitSound(self._screams[math.random(#self._screams)],75,130,0.8)
-					self._screamsNext = now+1
+					self:EmitSound(self._screams[math.random(#self._screams)], 75, 130, 0.8)
+					self._screamsNext = now + 1
 				end
 			end
 
@@ -90,23 +81,21 @@ function ENT:Touch(ent)
 
 		self:Consume(ent)
 	end
-end
+end]]
 
 function ENT:Consume(pl)
 	self:SetAllowTouch(false)
 	if self._consumed then return end
 	self._consumed = true
-
-	ms.Ores.GivePlayerOre(pl,self:GetRarity(),1)
-
-	self:EmitSound("physics/concrete/concrete_impact_soft3.wav",75,math.random(75,95))
-	self:EmitSound("physics/glass/glass_cup_break1.wav",70,math.random(190,210),0.75)
+	ms.Ores.GivePlayerOre(pl, self:GetRarity(), 1)
+	self:EmitSound("physics/concrete/concrete_impact_soft3.wav", 75, math.random(75, 95))
+	self:EmitSound("physics/glass/glass_cup_break1.wav", 70, math.random(190, 210), 0.75)
 	self:Remove()
 end
 
-function ENT:AllowGracePeriod(pl,dur)
+function ENT:AllowGracePeriod(pl, dur)
 	self.GraceOwner = pl or NULL
-	self.GraceOwnerExpiry = CurTime()+(dur or 10)
+	self.GraceOwnerExpiry = CurTime() + (dur or 10)
 end
 
 function ENT:SetAllowTouch(b)
@@ -123,10 +112,9 @@ function ENT:Think()
 			local pos = self:GetPos()
 			local dist = 90000
 
-			for k,v in next,player.GetHumans() do
+			for k, v in next, player.GetHumans() do
 				if pos:DistToSqr(v:GetPos()) <= dist then
-					self.Expiry = now+30
-
+					self.Expiry = now + 30
 					remove = false
 					break
 				end
@@ -138,7 +126,8 @@ function ENT:Think()
 		end
 	end
 
-	self:NextThink(now+1)
+	self:NextThink(now + 1)
+
 	return true
 end
 
@@ -150,13 +139,13 @@ function ENT:CheckExistence()
 		local pos = self:GetPos()
 
 		if not util.IsInWorld(pos) or util.TraceLine({
-			start = pos+(vector_up*2),
-			endpos = pos-(vector_up*128),
+			start = pos + (vector_up * 2),
+			endpos = pos - (vector_up * 128),
 			mask = MASK_SOLID_BRUSHONLY
 		}).HitNoDraw then
 			-- Outside the world or believed to be if there is nodraw underneath it
 			if self.GraceOwner and self.GraceOwner:IsValid() then
-				self:SetPos(self.GraceOwner:GetPos()+(vector_up*4))
+				self:SetPos(self.GraceOwner:GetPos() + (vector_up * 4))
 			else
 				SafeRemoveEntity(self)
 			end
--- lua/entities/mining_rock/init.lua
+++ lua/entities/mining_rock/init.lua
@@ -1,30 +1,25 @@
 include("shared.lua")
 AddCSLuaFile("shared.lua")
 AddCSLuaFile("cl_init.lua")
-
 ENT.ms_nogoto = "No cheating!"
 ENT._initialized = false
 ENT._nextDamaged = 0
 
-local function getOreRarity(baseRarity,magicFindChance)
+local function getOreRarity(baseRarity, magicFindChance)
 	if magicFindChance and math.random() <= magicFindChance then
 		local rSettings = ms.Ores.__R[baseRarity]
-
-		if rSettings.NextRarityId and ms.Ores.__R[rSettings.NextRarityId] then
-			return rSettings.NextRarityId
-		end
+		if rSettings.NextRarityId and ms.Ores.__R[rSettings.NextRarityId] then return rSettings.NextRarityId end
 	end
 
 	return baseRarity
 end
 
-local function createOre(pos,owner,rarity,magicFindChance,foolsDay)
-	local oreRarity = getOreRarity(rarity,magicFindChance)
+local function createOre(pos, owner, rarity, magicFindChance, foolsDay)
+	local oreRarity = getOreRarity(rarity, magicFindChance)
 	local foolsTime = foolsDay and math.random() <= 0.2
-
 	local ore = ents.Create(foolsTime and "mining_ore_fools" or "mining_ore")
 	ore:SetRarity(oreRarity)
-	ore:AllowGracePeriod(owner,20)
+	ore:AllowGracePeriod(owner, 20)
 	ore:SetPos(pos)
 	ore:SetAngles(AngleRand())
 
@@ -34,12 +29,11 @@ local function createOre(pos,owner,rarity,magicFindChance,foolsDay)
 	end
 
 	ore:Spawn()
-
 	local ophys = ore:GetPhysicsObject()
-	if ophys:IsValid() then
-		local vec = VectorRand() * math.random(64,128)
-		vec.z = math.abs(vec.z)
 
+	if ophys:IsValid() then
+		local vec = VectorRand() * math.random(64, 128)
+		vec.z = math.abs(vec.z)
 		ophys:AddVelocity(vec)
 		ophys:EnableCollisions(false)
 		ophys:EnableGravity(false)
@@ -49,12 +43,14 @@ local function createOre(pos,owner,rarity,magicFindChance,foolsDay)
 		function ore:Think()
 			if not IsValid(owner) or not owner:IsPlayer() then
 				SafeRemoveEntity(self)
+
 				return
 			end
 
 			local curPos = self:GetPos()
 			local targetPos = owner:WorldSpaceCenter()
 			local phys = self:GetPhysicsObject()
+
 			if IsValid(phys) then
 				phys:SetVelocity((targetPos - curPos):GetNormalized() * 1000)
 			end
@@ -66,9 +62,9 @@ local function createOre(pos,owner,rarity,magicFindChance,foolsDay)
 		end
 	end
 
-	if rarity != oreRarity then
+	if rarity ~= oreRarity then
 		-- Sound for Magic Find
-		ore:EmitSound(")ambient/levels/citadel/portal_beam_shoot1.wav",68,math.random(185,195),0.75)
+		ore:EmitSound(")ambient/levels/citadel/portal_beam_shoot1.wav", 68, math.random(185, 195), 0.75)
 	end
 
 	return ore
@@ -76,28 +72,26 @@ end
 
 function ENT:Initialize()
 	local rSettings = ms.Ores.__R[self:GetRarity()]
+
 	if not rSettings then
 		-- This isn't a registered rarity, clean it up!
 		SafeRemoveEntity(self)
+
 		return
 	end
 
 	self._initialized = true
-
-	local size = math.Clamp(self:GetSize(),0,2)
+	local size = math.Clamp(self:GetSize(), 0, 2)
 	local modelTable = self.Models[self:GetSizeName(size)] or self.Models.Small
-	local model = modelTable[math.random(1,#modelTable)]
-
+	local model = modelTable[math.random(1, #modelTable)]
 	self:SetModel(model.Mdl)
-
 	self:SetOffsetPos(model.Offset)
-	self:SetPos(self:GetPos()+self:GetRotatedOffset())
-
+	self:SetPos(self:GetPos() + self:GetRotatedOffset())
 	self:PhysicsInit(SOLID_VPHYSICS)
 	self:SetSolid(SOLID_VPHYSICS)
 	self:SetMoveType(MOVETYPE_VPHYSICS)
-
 	self.PhysObject = self:GetPhysicsObject()
+
 	if self.PhysObject:IsValid() then
 		self.PhysObject:SetMass(255)
 		self.PhysObject:EnableMotion(false)
@@ -107,28 +101,33 @@ function ENT:Initialize()
 
 	self:DrawShadow(false)
 	self:SetMaterial("models/shadertest/shader2")
-
 	self.ms_notouch = true
-
-	self:SetMaxHealthEx(math.ceil(rSettings.Health*(1+(size*0.5))))
+	self:SetMaxHealthEx(math.ceil(rSettings.Health * (1 + (size * 0.5))))
 	self:SetHealthEx(self:GetMaxHealthEx())
 
 	if (self:GetBonusSpotCount() or 0) > 0 then
-		self:SetBonusSpotSeed(math.random(0,1000))
-		self.BonusSpots = {Confirmed = {},Raw = {}}
+		self:SetBonusSpotSeed(math.random(0, 1000))
+
+		self.BonusSpots = {
+			Confirmed = {},
+			Raw = {}
+		}
 	end
 
-	timer.Simple(1.25,function()
-		if self:IsValid() then self:CheckExistence() end
+	timer.Simple(1.25, function()
+		if self:IsValid() then
+			self:CheckExistence()
+		end
 	end)
 end
 
 -- I really can't think of anything else better to check if something is in the caves or not...
 function ENT:IsStuckEx()
 	if not util.IsInWorld(self:GetPos()) or not util.IsInWorld(self:WorldSpaceCenter()) then return true end
-
 	local rock_pos = self:WorldSpaceCenter()
-	local dirs = { self:GetForward(), -self:GetForward(), self:GetRight(), -self:GetRight(), self:GetUp(), -self:GetUp() }
+
+	local dirs = {self:GetForward(), -self:GetForward(), self:GetRight(), -self:GetRight(), self:GetUp(), -self:GetUp()}
+
 	for _, dir in ipairs(dirs) do
 		local tr = util.TraceLine({
 			start = rock_pos,
@@ -156,27 +155,24 @@ end
 
 function ENT:OnTakeDamage(dmg)
 	local now = CurTime()
-
 	if self._nextDamaged > now then return end
-	self._nextDamaged = now+0.015
-
+	self._nextDamaged = now + 0.015
 	local attacker = dmg:GetAttacker()
 	if not (attacker:IsValid() and attacker:IsPlayer()) then return end
 	if attacker._miningBlocked or (attacker._miningCooldown and attacker._miningCooldown > now) then return end
 	if attacker.IsAFK and attacker:IsAFK() then return end
 	if attacker:GetShootPos():DistToSqr(dmg:GetDamagePosition()) > 40000 then return end
-
 	local isPickaxe = false
-
 	local wep = attacker:GetActiveWeapon()
 	if not wep:IsValid() then return end
+
 	if wep:GetClass() == "weapon_crowbar" then
 		-- Allowed, crowbar penalty
-		dmg:SetDamage(math.floor(dmg:GetDamage()*0.6))
+		dmg:SetDamage(math.floor(dmg:GetDamage() * 0.6))
 
 		if ms.Ores.SendChatMessage and (not attacker._miningCrowbarMsgNext or now > attacker._miningCrowbarMsgNext) then
-			ms.Ores.SendChatMessage(attacker,0,"Psst! You should use the Mining Pickaxe to get the most out of mining! Look in the Weapons tab!")
-			attacker._miningCrowbarMsgNext = now+120
+			ms.Ores.SendChatMessage(attacker, 0, "Psst! You should use the Mining Pickaxe to get the most out of mining! Look in the Weapons tab!")
+			attacker._miningCrowbarMsgNext = now + 120
 		end
 	elseif wep:GetClass() == "mining_pickaxe" then
 		-- Allowed
@@ -187,136 +183,127 @@ function ENT:OnTakeDamage(dmg)
 
 	-- Inflictor is either yourself (inflictor w/ crowbar = yourself) or the weapon
 	local inflictor = dmg:GetInflictor()
-	if inflictor != attacker and inflictor != wep then return end
+	if inflictor ~= attacker and inflictor ~= wep then return end
+
 	if attacker:GetMoveType() == MOVETYPE_NOCLIP then
-		self:EmitSound("player/suit_denydevice.wav",70)
+		self:EmitSound("player/suit_denydevice.wav", 70)
+
 		return
 	end
 
 	-- Check for and apply 1 second "last attacker" grace to prevent "rock stealing"
-	if self._lastAttacker != attacker and (self._lastAttackerGrace and self._lastAttackerGrace > now) then return end
+	if self._lastAttacker ~= attacker and (self._lastAttackerGrace and self._lastAttackerGrace > now) then return end
 	self._lastAttacker = attacker
-	self._lastAttackerGrace = now+1
-
-	local hp = self:GetHealthEx()-dmg:GetDamage()
+	self._lastAttackerGrace = now + 1
+	local hp = self:GetHealthEx() - dmg:GetDamage()
 	self:SetHealthEx(hp)
-
 	local rarity = self:GetRarity()
 	local magicFindChance = wep.Stats and wep.Stats.MagicFindChance
-
 	local foolsDay = ms.Ores.SpecialDays and ms.Ores.SpecialDays.ActiveId and ms.Ores.SpecialDays.Days[ms.Ores.SpecialDays.ActiveId].Name == "April Fools"
-
 	local dmgPos = dmg:GetDamagePosition()
+
 	if self:GetBonusSpotCount() > 0 then
 		local pos = self:GetPos()
 		local ang = self:GetAngles()
 		local isDirectDmg = not dmg:IsDamageType(DMG_NEVERGIB) -- DMG_NEVERGIB means it came from Shockwave ability
 
 		if isDirectDmg then
-			for k,v in next,self.BonusSpots.Confirmed do
+			for k, v in next, self.BonusSpots.Confirmed do
 				if v.Hit then continue end
-
 				local spotOffset = Vector(v.Pos)
 				spotOffset:Rotate(ang)
-
-				local spotPos = pos+spotOffset
-
+				local spotPos = pos + spotOffset
 				local hit = dmgPos:DistToSqr(spotPos) <= 10
+
 				if not hit then
 					local spotNormal = Vector(v.Normal)
 					spotNormal:Rotate(ang)
-
-					hit = (dmgPos-spotPos):GetNormalized():Dot(spotNormal) >= 0.9
+					hit = (dmgPos - spotPos):GetNormalized():Dot(spotNormal) >= 0.9
 				end
 
 				if hit then
 					v.Hit = true
-					self:SetBonusSpotHit(bit.bor(2^k,self:GetBonusSpotHit()))
-
-					self:EmitSound("physics/metal/metal_grenade_impact_hard2.wav",70,math.random(20,30))
-
-					createOre(dmgPos,attacker,rarity,magicFindChance,foolsDay)
+					self:SetBonusSpotHit(bit.bor(2 ^ k, self:GetBonusSpotHit()))
+					self:EmitSound("physics/metal/metal_grenade_impact_hard2.wav", 70, math.random(20, 30))
+					createOre(dmgPos, attacker, rarity, magicFindChance, foolsDay)
 				end
 			end
 		end
 	end
 
 	if hp > 0 then return end
-
 	-- Update _miningCooldown to allow checking when they last mined
 	attacker._miningCooldown = now
-
-	local size = math.Clamp(self:GetSize(),0,2)
-
-	self:EmitSound(")physics/concrete/boulder_impact_hard"..math.random(1,4)..".wav",70,105+(math.random(25,50)*(3-size)))
-	self:EmitSound(")physics/glass/glass_impact_bullet"..math.random(1,3)..".wav",75,math.random(160,200))
-
+	local size = math.Clamp(self:GetSize(), 0, 2)
+	self:EmitSound(")physics/concrete/boulder_impact_hard" .. math.random(1, 4) .. ".wav", 70, 105 + (math.random(25, 50) * (3 - size)))
+	self:EmitSound(")physics/glass/glass_impact_bullet" .. math.random(1, 3) .. ".wav", 75, math.random(160, 200))
 	local bonusAmount = 0
+
 	if isPickaxe then
 		local bonusChance = wep.Stats and wep.Stats.BonusChance or 0
-		bonusAmount = math.floor(bonusChance)+(math.random() <= bonusChance%1 and 1 or 0)
+		bonusAmount = math.floor(bonusChance) + (math.random() <= bonusChance % 1 and 1 or 0)
 	end
 
-	for i=1,1+bonusAmount do
+	for i = 1, 1 + bonusAmount do
 		if i > 1 then
-			timer.Simple(i*0.175,function()
-				local ore = createOre(dmgPos,attacker,rarity,magicFindChance,foolsDay)
-				ore:EmitSound(")garrysmod/save_load4.wav",70,152+(i*12))
+			timer.Simple(i * 0.175, function()
+				local ore = createOre(dmgPos, attacker, rarity, magicFindChance, foolsDay)
+				ore:EmitSound(")garrysmod/save_load4.wav", 70, 152 + (i * 12))
 			end)
 		else
-			createOre(dmgPos,attacker,rarity,magicFindChance,foolsDay)
+			createOre(dmgPos, attacker, rarity, magicFindChance, foolsDay)
 		end
 	end
 
 	if size > 0 and not self.PreventSplit then
 		local pos = self:GetCorrectedPos()
 		local force = dmg:GetDamageForce():Angle():Right()
-
 		local rocks = {}
 		local numRocks = (math.random() <= (wep.Stats and wep.Stats.FineCutChance or 0)) and 3 or 2
 
-		for i=0,numRocks-1 do
+		for i = 0, numRocks - 1 do
 			local r = ents.Create(self:GetClass())
-			r:SetSize(size-1)
+			r:SetSize(size - 1)
 			r:SetRarity(rarity)
-
 			r:SetPos(pos)
 			r:SetAngles(AngleRand())
-
 			r:Spawn()
+
 			if ms.Ores.SpawnedRocks and ms.Ores.SpawnedRocks[self] then
 				ms.Ores.SpawnedRocks[r] = true
 			end
 
 			local phys = r:GetPhysicsObject()
+
 			if phys:IsValid() then
 				phys:EnableMotion(true)
 				phys:Wake()
-				phys:AddVelocity((force*48*math.cos(i/(numRocks-1)*math.pi))+(vector_up*16)+(VectorRand()*64))
+				phys:AddVelocity((force * 48 * math.cos(i / (numRocks - 1) * math.pi)) + (vector_up * 16) + (VectorRand() * 64))
 			end
 
-			rocks[#rocks+1] = r
+			rocks[#rocks + 1] = r
 		end
 
 		-- Sound for Precise Cut
 		if numRocks > 2 then
-			self:EmitSound(")ambient/machines/slicer3.wav",70,math.random(40,60))
+			self:EmitSound(")ambient/machines/slicer3.wav", 70, math.random(40, 60))
 		end
 
 		-- Briefly nocollide all the rocks that spawn
-		for k,v in next,rocks do
-			for _,x in next,rocks do
+		for k, v in next, rocks do
+			for _, x in next, rocks do
 				if v == x then continue end
+				local nocollide = constraint.NoCollide(v, x, 0, 0)
 
-				local nocollide = constraint.NoCollide(v,x,0,0)
-				if nocollide then SafeRemoveEntityDelayed(nocollide,0.75) end
+				if nocollide then
+					SafeRemoveEntityDelayed(nocollide, 0.75)
+				end
 			end
 		end
 	end
 
-	hook.Run("PlayerDestroyedMiningRock",attacker,self)
-
-	SafeRemoveEntity(self)	-- Possible some naughty hook removes self for us
+	hook.Run("PlayerDestroyedMiningRock", attacker, self)
+	SafeRemoveEntity(self) -- Possible some naughty hook removes self for us
 end
 
 function ENT:OnRemove()
@@ -330,7 +317,8 @@ function ENT:Think()
 		self.PhysObject:EnableMotion(false)
 	end
 
-	self:NextThink(CurTime()+1)
+	self:NextThink(CurTime() + 1)
+
 	return true
 end
 
@@ -343,7 +331,7 @@ function ENT:CheckExistence()
 
 		if not util.IsInWorld(pos) or util.TraceLine({
 			start = pos,
-			endpos = pos-(vector_up*128),
+			endpos = pos - (vector_up * 128),
 			mask = MASK_SOLID_BRUSHONLY
 		}).HitNoDraw then
 			-- Outside the world or believed to be if there is nodraw underneath it
@@ -355,22 +343,20 @@ end
 -- We're doing it like this because physics meshes suck, we want the spot to actually be on the model
 -- Hitting spots will be verified on the server of course (the malicious client would have to try quite hard to cheat it)
 util.AddNetworkString("mining_rock.BonusSpot")
-net.Receive("mining_rock.BonusSpot",function(_,pl)
-	if not pl:IsValid() then return end
 
+net.Receive("mining_rock.BonusSpot", function(_, pl)
+	if not pl:IsValid() then return end
 	local ent = net.ReadEntity()
 	if not (ent and ent:IsValid() and ent:GetClass() == "mining_rock") then return end
 	if not ent.BonusSpots then return end
-
 	local maxSpots = ent.GetBonusSpotCount and ent:GetBonusSpotCount()
 	if not maxSpots or maxSpots <= (ent.BonusSpots.Raw[pl] and #ent.BonusSpots.Raw[pl] or 0) then return end
-
 	ent.BonusSpots.Raw[pl] = ent.BonusSpots.Raw[pl] or {}
 
-	for i=1,maxSpots do
+	for i = 1, maxSpots do
 		local pos = net.ReadVector()
 		local normal = net.ReadVector()
-		local id = #ent.BonusSpots.Raw[pl]+1
+		local id = #ent.BonusSpots.Raw[pl] + 1
 
 		ent.BonusSpots.Raw[pl][id] = {
 			Pos = pos,
@@ -378,15 +364,15 @@ net.Receive("mining_rock.BonusSpot",function(_,pl)
 		}
 
 		if ent.BonusSpots.Confirmed[id] and ent.BonusSpots.Confirmed[id].Hit then continue end
+		local best, bestPos, bestNor, bestV = {}, nil, nil, 0
 
-		local best,bestPos,bestNor,bestV = {},nil,nil,0
-		for k,v in next,ent.BonusSpots.Raw do
+		for k, v in next, ent.BonusSpots.Raw do
 			local rawData = v[id]
 			local strPos = tostring(rawData.Pos)
-			best[strPos] = (best[strPos] or 0)+1
+			best[strPos] = (best[strPos] or 0) + 1
 
 			if best[strPos] >= bestV then
-				bestPos,bestNor,bestV = rawData.Pos,rawData.Normal,best[strPos]
+				bestPos, bestNor, bestV = rawData.Pos, rawData.Normal, best[strPos]
 			end
 		end
 
