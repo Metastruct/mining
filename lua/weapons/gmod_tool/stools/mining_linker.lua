dbc6fa6e0b38b91e6dd715275b945f4fe2182bd6
Ryan <ryan.rbsr@gmail.com>
2024-09-12 18:47:59 +0200

    show the toolgun beam

--- lua/weapons/gmod_tool/stools/mining_linker.lua
+++ lua/weapons/gmod_tool/stools/mining_linker.lua
@@ -9,22 +9,22 @@ TOOL.Command		= nil
 TOOL.ConfigName		= ""
 
 function TOOL:LeftClick(tr)
-	if not CLIENT then return end
-	if not IsValid(tr.Entity) then return end
+	if not CLIENT then return false end
+	if not IsValid(tr.Entity) then return false end
 
 	local ent = tr.Entity
 	if ent.CPPIGetOwner and ent:CPPIGetOwner() ~= LocalPlayer() then
 		surface.PlaySound("buttons/button8.wav")
-		return
+		return false
 	end
 
 	local interfaces = self.SelectedOutput and _G.MA_Orchestrator.GetInputs(ent) or _G.MA_Orchestrator.GetOutputs(ent)
 	local interface_count = table.Count(interfaces)
-	if interface_count == 0 then return end
+	if interface_count == 0 then return false end
 
 	local cur_index = (self.CurrentIndex % table.Count(interfaces)) + 1
 	local interface_data = interfaces[cur_index]
-	if not interface_data then return end
+	if not interface_data then return false end
 
 	if not self.SelectedOutput then
 		self.SelectedOutput = interface_data
@@ -32,7 +32,7 @@ function TOOL:LeftClick(tr)
 	else
 		if self.SelectedOutput.Type ~= interface_data.Type then
 			surface.PlaySound("buttons/button8.wav")
-			return
+			return false
 		end
 
 		_G.MA_Orchestrator.Link(self.SelectedOutput, interface_data)
@@ -40,54 +40,60 @@ function TOOL:LeftClick(tr)
 
 		surface.PlaySound("buttons/button4.wav")
 	end
+
+	return true
 end
 
 function TOOL:RightClick(tr)
-	if not CLIENT then return end
-	if not IsValid(tr.Entity) then return end
+	if not CLIENT then return false end
+	if not IsValid(tr.Entity) then return false end
 
 	local ent = tr.Entity
 	if ent.CPPIGetOwner and ent:CPPIGetOwner() ~= LocalPlayer() then
 		surface.PlaySound("buttons/button8.wav")
-		return
+		return false
 	end
 
 	local interfaces = self.SelectedOutput and _G.MA_Orchestrator.GetInputs(ent) or _G.MA_Orchestrator.GetOutputs(ent)
 	local interface_count = table.Count(interfaces)
-	if interface_count == 0 then return end
+	if interface_count == 0 then return false end
 
 	self.CurrentIndex = self.CurrentIndex + 1
 	surface.PlaySound("ui/buttonrollover.wav")
+
+	return true
 end
 
 function TOOL:Reload(tr)
-	if not CLIENT then return end
+	if not CLIENT then return false end
 
 	if self.SelectedOutput then
 		self.SelectedOutput = nil
 		surface.PlaySound("ui/buttonclickrelease.wav")
-		return
+		return false
 	end
 
-	if not IsValid(tr.Entity) then return end
+	if not IsValid(tr.Entity) then return false end
 
 	local ent = tr.Entity
 	if ent.CPPIGetOwner and ent:CPPIGetOwner() ~= LocalPlayer() then
 		surface.PlaySound("buttons/button8.wav")
-		return
+		return false
 	end
 
 	local outputs = _G.MA_Orchestrator.GetOutputs(ent)
 	local outputs_count = table.Count(outputs)
-	if outputs_count == 0 then return end
+	if outputs_count == 0 then return false end
 
 	local cur_index = (self.CurrentIndex % table.Count(outputs)) + 1
 
 	local output_data = outputs[cur_index]
-	if not output_data then return end
+	if not output_data then return false end
 
 	_G.MA_Orchestrator.Unlink(true, output_data)
 	surface.PlaySound("ui/buttonclickrelease.wav")
+
+	return true
 end
 
 if CLIENT then
