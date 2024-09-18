c8a4d48e1abbf60ea17aabed12dfb7ed9bc6ccb4
Ryan <ryan.rbsr@gmail.com>
2024-09-18 17:18:30 +0200

    tiny offset for people with non nvidia cards

--- lua/entities/ma_terminal.lua
+++ lua/entities/ma_terminal.lua
@@ -100,7 +100,7 @@ if CLIENT then
 		ang:RotateAroundAxis(self:GetForward(), -90)
 		ang:RotateAroundAxis(self:GetRight(), 4.5)
 
-		cam.Start3D2D(self:GetPos() + self:GetRight() * 9.4 + self:GetForward() * 11.7 + self:GetUp() * 12, ang, scale / 1.5)
+		cam.Start3D2D(self:GetPos() + self:GetRight() * 9.4 + self:GetForward() * 11.72 + self:GetUp() * 12, ang, scale / 1.5)
 			self:WaitScreen(w, h)
 		cam.End3D2D()
 	end
