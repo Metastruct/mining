2651ebc2e453729918303f4aca8d9cb7cd39b41d
Ryan <ryan.rbsr@gmail.com>
2024-09-22 18:08:04 +0200

    this should help

--- lua/includes/modules/ma_orchestrator.lua
+++ lua/includes/modules/ma_orchestrator.lua
@@ -1,4 +1,5 @@
 -- this is needed for entities to have access to orchestrator.RegisterInput and orchestrator.RegisterOutput
+AddCSLuaFile()
 
 if type(_G.MA_Orchestrator) ~= "table" then
 	include("mining/logic/automation/sh_orchestrator.lua")
